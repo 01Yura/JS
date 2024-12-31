f002fbe57765923f78012f2c44c6cd78c213e9b1
Yura Primyshev <p020885@yandex.ru>
2024-12-31 11:47:26 -0500

    8

--- 1.js
+++ 1.js
@@ -7,6 +7,12 @@
 //colors.push("gray")
 //document.write(colors)
 
+/*
 let colors = ["red", "green", "black"]
 colors.push("gray")
 document.write(colors[0], " ", colors[3])
+*/
+
+"use strict";
+let = "Векна";
+console.log(creature);
